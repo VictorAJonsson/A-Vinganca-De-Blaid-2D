7a5ba3686451f5088919cf65fa0aa4250028f5bd
VictorAJonsson <victor4jonsson@gmail.com>
2025-01-08 13:23:59 -0300

    enemy dead animation

--- Assets/Scripts/Enemy/baseEnemy.cs
+++ Assets/Scripts/Enemy/baseEnemy.cs
@@ -1,17 +1,42 @@
+using System.Collections;
 using UnityEngine;
 
+// [RequireComponent(typeof(Animator), typeof(AudioSource))]
 [RequireComponent(typeof(Animator))]
+
 public abstract class BaseEnemy : MonoBehaviour
 {
     protected Animator animator;
+    // protected AudioSource audioSource;
+    protected Health health;
 
     protected bool canAttack = true;
+
     protected virtual void Awake()
     {
         animator = GetComponent<Animator>();
+        // audioSource = GetComponent<AudioSource>();
+        health = GetComponent<Health>();
 
+        health.OnHurt += PlayHurtAnim;
+        health.OnDead += HandleDeath;
     }
 
     protected abstract void Update();
 
+    private void PlayHurtAnim() => animator.SetTrigger("hurt");
+    
+    private void HandleDeath()
+    {
+        canAttack = false;
+        // GetComponent<BoxCollider2D>().enabled = false;
+        animator.SetTrigger("dead");
+        StartCoroutine(DestroyEnemy(2));
+    }
+
+    private IEnumerator DestroyEnemy(int time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(this.gameObject);
+    }
 }
